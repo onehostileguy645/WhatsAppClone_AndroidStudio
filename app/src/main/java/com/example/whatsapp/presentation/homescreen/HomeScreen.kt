dc1eb76e8e965ba1077d3a13006fd2c2ceb0913f
grazo <grazox645@gmail.com>
2025-07-11 22:00:54 +0500

    HomeScreen + ChatDesign + BottomNavBar + ChatDesignModel

--- app/src/main/java/com/example/whatsapp/presentation/homescreen/HomeScreen.kt
+++ app/src/main/java/com/example/whatsapp/presentation/homescreen/HomeScreen.kt
@@ -4,14 +4,12 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
