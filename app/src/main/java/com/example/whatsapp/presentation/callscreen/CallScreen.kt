4f3c4da5a36788673767734e7be50e7c3e4a38b2
grazo <grazox645@gmail.com>
2025-08-07 20:42:42 +0500

    CallScreen end

--- app/src/main/java/com/example/whatsapp/presentation/callscreen/CallScreen.kt
+++ app/src/main/java/com/example/whatsapp/presentation/callscreen/CallScreen.kt
@@ -9,8 +9,13 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -25,16 +30,36 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.whatsapp.R
+import com.example.whatsapp.presentation.bottomnavigation.BottomNavigation
 
 @Composable
 @Preview(showSystemUi = true)
 fun CallScreen(){
+
+
+    val sampleCall = listOf(
+        Call(R.drawable.ssa, "ssa", "Yesterday, 8:30 AM", true),
+        Call(R.drawable.ssa, "ssa", "Yesterday, 8:30 AM", true),
+        Call(R.drawable.ssa, "ssa", "Yesterday, 8:30 AM", true),
+        Call(R.drawable.ssa, "ssa", "Yesterday, 8:30 AM", true),
+        Call(R.drawable.ssa, "ssa", "Yesterday, 8:30 AM", true),
+        Call(R.drawable.ssa, "ssa", "Yesterday, 8:30 AM", true),
+        Call(R.drawable.ssa, "ssa", "Yesterday, 8:30 AM", true),
+        Call(R.drawable.ssa, "ssa", "Yesterday, 8:30 AM", true),
+        Call(R.drawable.ssa, "ssa", "Yesterday, 8:30 AM", true),
+        Call(R.drawable.ssa, "ssa", "Yesterday, 8:30 AM", true),
+        Call(R.drawable.ssa, "ssa", "Yesterday, 8:30 AM", true)
+    )
+
+
+
     var isSearching by remember() {
         mutableStateOf(false)
     }
@@ -124,10 +149,50 @@ fun CallScreen(){
                 }
             }
         },
+
+        bottomBar = {
+            BottomNavigation()
+        },
+
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {/*TODO*/},
+                containerColor = colorResource(id = R.color.light_green),
+                modifier = Modifier.size(65.dp),
+                contentColor = Color.White
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.add_call),
+                    contentDescription = null,
+                )
+            }
+        },
     ){
         Column (modifier = Modifier.padding(it)){
             Spacer(modifier = Modifier.height(16.dp))
             FavoritesSection()
+
+            Button(
+                onClick = {},
+                colors = ButtonDefaults.buttonColors(containerColor = colorResource(R.color.light_green)),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                Text(text = "Start a new call", color = Color.White, fontSize = 16.sp, fontWeight = FontWeight.Bold)
+            }
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Text(text = "Recent Calls", fontSize = 20.sp, fontWeight = FontWeight.Bold, modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp))
+
+
+
+            LazyColumn {
+                items(sampleCall) { data ->
+                    CallItemDesign(data)
+                }
+            }
         }
     }
 
