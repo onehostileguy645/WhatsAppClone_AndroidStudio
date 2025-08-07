ce1e73c48bb17aae3dcb9eb5ea248027a9f9441a
grazo <grazox645@gmail.com>
2025-08-07 20:16:41 +0500

    updates

--- app/src/main/java/com/example/whatsapp/presentation/callscreen/CallScreen.kt
+++ app/src/main/java/com/example/whatsapp/presentation/callscreen/CallScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.DropdownMenu
@@ -124,7 +125,10 @@ fun CallScreen(){
             }
         },
     ){
-        Column (modifier = Modifier.padding(it)){  }
+        Column (modifier = Modifier.padding(it)){
+            Spacer(modifier = Modifier.height(16.dp))
+            FavoritesSection()
+        }
     }
 
 }
--- app/src/main/java/com/example/whatsapp/presentation/callscreen/FavoritesSection.kt
+++ app/src/main/java/com/example/whatsapp/presentation/callscreen/FavoritesSection.kt
@@ -1,29 +1,62 @@
 package com.example.whatsapp.presentation.callscreen
 
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.whatsapp.R
 
 @Composable
 @Preview(showSystemUi = true)
 fun FavoritesSection(){
-    Column {
-        Image(
-            painter = painterResource(id = R.drawable.ssa),
-            contentDescription = null,
-            modifier = Modifier
-                .size(60.dp)
-                .clip(shape = CircleShape),
-            contentScale = ContentScale.Crop
+
+    val sampleFavorites = listOf(
+        FavoritesContact(R.drawable.ssa, name = "Aliyev Vali"),
+        FavoritesContact(R.drawable.ssa, name = "Aliyev Vali"),
+        FavoritesContact(R.drawable.ssa, name = "Aliyev Vali"),
+        FavoritesContact(R.drawable.ssa, name = "Aliyev Vali"),
+        FavoritesContact(R.drawable.ssa, name = "Aliyev Vali"),
+        FavoritesContact(R.drawable.ssa, name = "Aliyev Vali"),
+        FavoritesContact(R.drawable.ssa, name = "Aliyev Vali"),
+        FavoritesContact(R.drawable.ssa, name = "Aliyev Vali"),
+        FavoritesContact(R.drawable.ssa, name = "Aliyev Vali"),
+        FavoritesContact(R.drawable.ssa, name = "Aliyev Vali"),
+        FavoritesContact(R.drawable.ssa, name = "Aliyev Vali"),
+        FavoritesContact(R.drawable.ssa, name = "Aliyev Vali"),
+        FavoritesContact(R.drawable.ssa, name = "Aliyev Vali"),
+        FavoritesContact(R.drawable.ssa, name = "Aliyev Vali"),
+        FavoritesContact(R.drawable.ssa, name = "Aliyev Vali")
+    )
+
+    Column (
+        modifier = Modifier.padding(start = 16.dp, bottom = 8.dp)
+    ){
+        Text(
+            "Favorites",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(bottom = 8.dp)
         )
+        Row (modifier = Modifier.fillMaxWidth().horizontalScroll(rememberScrollState())){
+            sampleFavorites.forEach {
+                FavoritesItem(it)
+            }
+        }
     }
 }
+
+
+data class FavoritesContact(
+    val image:Int,
+    val name:String
+)
